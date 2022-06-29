f029d7bbf48cce31792f8d39c2f3921438e314f4
DannySilv <danielesilvestre@outlook.it>
2022-06-29 16:48:40 +0200

    Migrated Trains Table

---
+++ database/migrations/2022_06_29_143459_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_hour', 5);
+            $table->time('arrival_hour', 5);
+            $table->date('departure_day');
+            $table->string('train_code', 20);
+            $table->unsignedTinyInteger('wagons_number');
+            $table->boolean('on_time')->default(1);
+            $table->boolean('cancelled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
